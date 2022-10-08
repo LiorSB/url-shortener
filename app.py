8d9d438730ee682b82b63bc36d45e62cd782f60b
LiorQM <lior.sb18@gmail.com>
2022-10-08 15:59:07 +0300

    Sharding + LB works.

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask_caching import Cache
 
 
-MONGODB_HOST = 'mongodb://localhost:27017/'
+# MONGODB_HOST = 'mongodb://localhost:27017/'
+MONGODB_HOST = 'mongodb://localhost:27117/'
 DB_NAME = 'url_shortner'
 COLLECTION_NAME = 'url'
 
@@ -12,7 +13,12 @@ app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
 mongo_client: pymongo.MongoClient = pymongo.MongoClient(MONGODB_HOST)
-# mongo_client.admin.command('enableSharding', DB_NAME)
-# mongo_client.admin.command('shardCollection', f'{DB_NAME}.{COLLECTION_NAME}), key=_id)')
+mongo_client.admin.command('enableSharding', DB_NAME)
+mongo_client.admin.command({
+    'shardCollection': f'{DB_NAME}.{COLLECTION_NAME}',
+    'key': {
+        '_id': 'hashed'
+    }
+})
 url_shortner_db = mongo_client[DB_NAME]
 url_collection = url_shortner_db[COLLECTION_NAME]
