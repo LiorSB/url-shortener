6fbfc637ef8d82bcdac450052ae7528d878d0dc8
Lior <92099051+LiorSB@users.noreply.github.com>
2022-10-10 11:15:49 +0300

    Update app.py

--- back_end/app.py
+++ back_end/app.py
@@ -1,5 +1,6 @@
 import pymongo
 from flask import Flask
+from flask_cors import CORS
 from flask_caching import Cache
 from .leader_election import LeaderElection
 
@@ -18,6 +19,7 @@ leader_election = LeaderElection(ZOOKEEPER_ADDRESS, ZNODE_NAME, ELECTION_PATH)
 leader_election.register()
 
 app = Flask(__name__)
+CORS(app)
 
 cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
