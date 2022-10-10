a25879a4e4aa4c414b0337c8030bf24a6ffcc6bd
LiorQM <lior.sb18@gmail.com>
2022-10-10 11:23:23 +0300

    added mock responses

--- back_end/views.py
+++ back_end/views.py
@@ -4,7 +4,7 @@ from urllib.parse import urljoin
 import validators
 from flask import request, make_response, redirect, jsonify, Response
 from .utils import hash_to_specified_length
-from .app import app, cache#url_collection, user_collection,
+from .app import app#, url_collection, user_collection, cache
 
 
 MAX_URL_LENGTH = 2048
@@ -98,64 +98,65 @@ def shorten_url(user_id: str = None) -> Response:
 
     # url_collection.insert_one(url_dictionary)
 
-    response = make_response(
+    return make_response(
         jsonify({
-            'short_url': urljoin(BASE_URL, url_hash)
+            'short_url': urljoin(BASE_URL, url_hash),
+            'original_url': url,
+            'creation_date': current_time.isoformat(),
+            'expiration_date': expiration_date.isoformat()
         }),
         HTTPStatus.OK
     )
 
-    return response
 
+# @app.get('/<suffix_hash>')
+# def redirect_url(suffix_hash: str) -> Response:
+#     """
+#     Redirect a short URL to its original URL.
 
-@app.get('/<suffix_hash>')
-def redirect_url(suffix_hash: str) -> Response:
-    """
-    Redirect a short URL to its original URL.
+#     Args:
+#         suffix_hash (str): _id of the URL.
 
-    Args:
-        suffix_hash (str): _id of the URL.
+#     Returns:
+#         Response: In case of success redirect to original URL, else failure message.
+#     """
+#     # In case the url exists in the cache.
+#     if url := cache.get(suffix_hash):
+#         return redirect(
+#             url,
+#             code=HTTPStatus.FOUND
+#         )
 
-    Returns:
-        Response: In case of success redirect to original URL, else failure message.
-    """
-    # In case the url exists in the cache.
-    if url := cache.get(suffix_hash):
-        return redirect(
-            url,
-            code=HTTPStatus.FOUND
-        )
+#     # In case the suffix_hash doesn't exist in the DB, return 404 message.
+#     if not (url_dictionary := url_collection.find_one({'_id': suffix_hash})):
+#         return make_response(
+#             jsonify({
+#                 'error': 'Unable to find URL to redirect to.'
+#             }),
+#             HTTPStatus.NOT_FOUND
+#         )
 
-    # In case the suffix_hash doesn't exist in the DB, return 404 message.
-    # if not (url_dictionary := url_collection.find_one({'_id': suffix_hash})):
-    #     return make_response(
-    #         jsonify({
-    #             'error': 'Unable to find URL to redirect to.'
-    #         }),
-    #         HTTPStatus.NOT_FOUND
-    #     )
+#     expiration_date = datetime.fromisoformat(url_dictionary['expiration_date'])
+#     current_time = datetime.now()
 
-    expiration_date = datetime.fromisoformat(url_dictionary['expiration_date'])
-    current_time = datetime.now()
+#     # In case the URL has expired, return 410 message.
+#     if current_time >= expiration_date:
+#         url_collection.delete_one(url_dictionary)
 
-    # In case the URL has expired, return 410 message.
-    # if current_time >= expiration_date:
-    #     url_collection.delete_one(url_dictionary)
+#         return make_response(
+#             jsonify({
+#                 'error': 'The shortened URL has expired.'
+#             }),
+#             HTTPStatus.GONE
+#         )
 
-    #     return make_response(
-    #         jsonify({
-    #             'error': 'The shortened URL has expired.'
-    #         }),
-    #         HTTPStatus.GONE
-    #     )
+#     url = url_dictionary['original_url']
+#     cache.set(suffix_hash, url)
 
-    url = url_dictionary['original_url']
-    cache.set(suffix_hash, url)
-
-    return redirect(
-        url,
-        code=HTTPStatus.FOUND
-    )
+#     return redirect(
+#         url,
+#         code=HTTPStatus.FOUND
+#     )
 
 
 @app.post('/sign_up')
@@ -210,11 +211,56 @@ def sign_up() -> Response:
         'creation_date': datetime.now().isoformat()
     }
 
-    user_id = 'a'#user_collection.insert_one(user_dictionary).inserted_id
+    # user_id = user_collection.insert_one(user_dictionary).inserted_id
 
-    return redirect(
-        urljoin(BASE_URL, user_id),
-        HTTPStatus.FOUND
+    return make_response(
+        jsonify({
+            'user_id': str(123456789),
+            **user_dictionary
+        }),
+        HTTPStatus.OK
+    )
+
+
+@app.post('/log_in')
+def log_in() -> Response:
+    request_body = request.json
+
+    # In case of no body.
+    if not request_body:
+        return make_response(
+            jsonify({
+                'error': 'No valid JSON data has been received.'
+            }),
+            HTTPStatus.BAD_REQUEST
+        )
+
+    # In case there is no email in the body.
+    if not (email := request_body.get('email')):
+        return make_response(
+            jsonify({
+                'error': 'email key is missing from JSON data.'
+            }),
+            HTTPStatus.BAD_REQUEST
+        )
+
+    # In case the email already exists in the DB.
+    # if not (user_dictionary := user_collection.find_one({'email': email})):
+    #     return make_response(
+    #         jsonify({
+    #             'error': 'The email does not exist'
+    #         }),
+    #         HTTPStatus.NOT_FOUND
+    #     )
+
+    return make_response(
+        jsonify({
+            'uer_id': '123456789',
+            'name': 'lior',
+            'email': request_body['email'],
+            'creation_date': 'now',
+        }),
+        HTTPStatus.OK
     )
 
 
@@ -239,11 +285,33 @@ def get_user_urls(user_id: str = None) -> Response:
             HTTPStatus.FORBIDDEN
         )
 
-    url_dictionaries = 'a'#list(url_collection.find({'user_id': user_id}))
+    # url_dictionaries = url_collection.find({'user_id': user_id})
+
+    # user_urls = [
+    #     {
+    #         'short_url': urljoin(BASE_URL, url_dictionary['_id']),
+    #         'original_url': url_dictionary['original_url'],
+    #         'creation_date': url_dictionary['creation_date'],
+    #         'expiration_date': url_dictionary['expiration_date'],
+    #     } for url_dictionary in url_dictionaries
+    # ]
 
     return make_response(
         jsonify({
-            'url_data': url_dictionaries
+            'url_data': [
+                {
+                    "short_url": "http://127.0.0.1/5gDZEF",
+                    "original_url": "https://github.com/LiorSB/url-shortener/tree/feature/backend-db-ds",
+                    "creation_date": "2022-10-08T23:14:47.558657",
+                    "expiration_date": "2027-10-07T23:14:47.558657"
+                },
+                {
+                    "short_url": "http://127.0.0.1/1bqOQw",
+                    "original_url": "https://www.w3schools.com/python/python_mongodb_find.asp",
+                    "creation_date": "2022-10-08T23:56:03.839908",
+                    "expiration_date": "2027-10-07T23:56:03.839908"
+                }
+            ]
         }),
         HTTPStatus.OK
     )
