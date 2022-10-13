ca42114e5922bd8b77428ebfc2a2def6c40b91d1
LiorQM <lior.sb18@gmail.com>
2022-10-13 23:25:13 +0300

    Finalized project

--- back_end/app.py
+++ back_end/app.py
@@ -8,9 +8,7 @@ MONGODB_HOST = 'mongodb://localhost:27017/'
 DB_NAME = 'url_shortener'
 URL_COLLECTION_NAME = 'url'
 USER_COLLECTION_NAME = 'user'
-ZOOKEEPER_ADDRESS = 'localhost:2181'
-ZNODE_NAME = 'views'
-ELECTION_PATH = '/election'
+SHARD_STRINGS = ('enableSharding', 'shardCollection')
 
 app = Flask(__name__)
 CORS(app)
@@ -19,19 +17,23 @@ cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
 # Start MongoDB and enable sharding by hash.
 mongo_client: pymongo.MongoClient = pymongo.MongoClient(MONGODB_HOST)
-mongo_client.admin.command('enableSharding', DB_NAME)
-mongo_client.admin.command({
-    'shardCollection': f'{DB_NAME}.{URL_COLLECTION_NAME}',
-    'key': {
-        '_id': 'hashed'
-    }
-})
-mongo_client.admin.command({
-    'shardCollection': f'{DB_NAME}.{USER_COLLECTION_NAME}',
-    'key': {
-        '_id': 'hashed'
-    }
-})
+
+if all(shard_string in str(mongo_client.admin.command('listCommands')) for shard_string in SHARD_STRINGS):
+    mongo_client.admin.command('enableSharding', DB_NAME)
+
+    mongo_client.admin.command({
+        'shardCollection': f'{DB_NAME}.{URL_COLLECTION_NAME}',
+        'key': {
+            '_id': 'hashed'
+        }
+    })
+    mongo_client.admin.command({
+        'shardCollection': f'{DB_NAME}.{USER_COLLECTION_NAME}',
+        'key': {
+            '_id': 'hashed'
+        }
+    })
+
 url_shortener_db = mongo_client[DB_NAME]
 url_collection = url_shortener_db[URL_COLLECTION_NAME]
 user_collection = url_shortener_db[USER_COLLECTION_NAME]
